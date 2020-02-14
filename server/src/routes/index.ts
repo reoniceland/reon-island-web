ac59248212555ab3bb3c56aa7a5e3cff927e1fec
Arnar Yngvason <arnar@reon.is>
2020-02-14 15:00:43 +0000

    Added an example of working with an external REST API

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,7 @@ import Router from 'koa-router'
 
 import restExample from './restExample'
 import soapExample from './soapExample'
+import graphqlExample from './graphqlExample'
 
 
 const router = new Router()
@@ -14,5 +15,6 @@ router.get('/', (ctx, next) => {
 
 router.get('/rest/', restExample)
 router.get('/soap/', soapExample)
+router.get('/graphql-example/', graphqlExample)
 
 export default router
--- server/src/routes/restExample.ts
+++ server/src/routes/restExample.ts
@@ -1,9 +1,62 @@
 import Router from 'koa-router'
 import axios from 'axios'
+import { fromPairs, zip, pick } from 'lodash'
 
 
+interface Reflection<T> {
+  value?: T
+  error?: any
+  status: 'resolved' | 'rejected'
+}
+
+function reflect<T>(promise: Promise<T>): Promise<Reflection<T>> {
+  return promise.then(
+    (value) => ({ value, status: 'resolved' }),
+    (error) => ({ error, status: 'rejected' }),
+  )
+}
+
+
+/**
+ * Gets the results from a few different currency APIs.
+ * Will always return results even if some or all of the
+ * external APIs fail to deliver.
+ */
 const restExample: Router.IMiddleware = async (ctx, next) => {
-  ctx.body = 'TODO'
+  const results = await Promise.all([
+    axios.get('http://apis.is/currency/m5'),
+    axios.get('http://apis.is/currency/arion'),
+    axios.get('http://apis.is/currency/lb'),
+  ].map(reflect))
+
+  const dataOut = fromPairs(zip(
+    ['m5', 'arion', 'lb'],
+    results.map(({ value, error }) => {
+      if (error == null) {
+        return value.data.results
+      }
+
+      if (error.response) {
+        return {
+          error: 'remote_server_error',
+          response: pick(error.response, ['data', 'status', 'headers']),
+        }
+      } else if (error.request) {
+        return {
+          error: 'remote_server_error',
+          response: null,
+        }
+      }
+
+      console.error(error)
+      return {
+        error: 'internal_server_error',
+      }
+    }),
+  ))
+
+  ctx.body = JSON.stringify(dataOut, null, 2)
+
   next()
 }
 
