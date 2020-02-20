5900a1e8cf58670da72e381cebde16c7d26ef0b2
Arnar Yngvason <arnar@reon.is>
2020-02-20 15:25:24 +0000

    Cypress setup with some simple integration tests

---
+++ cypress/integration/base.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+/* eslint-disable no-undef */
+
+it('Finds foreldraorlof page', () => {
+  cy.visit('/')
+  cy.get('[href="/þjónustur/fjölskyldan"]').should('be.visible').click()
+  cy.get('.page-nav__route-item:contains("Barneignir")').should('be.visible').click()
+  cy.get('.page-nav__sub-route-item:contains("Fæðingar- og foreldraorlof")').should('be.visible').click()
+  cy.get('.text--h2').contains('Fæðingar- og foreldraorlof')
+})
+
+it('Finds foreldraorlof page from "Sjá allt"', () => {
+  cy.visit('/')
+  cy.get('.title-wrapper__link:contains("Sjá allt")').should('be.visible').click()
+  cy.get('.page-nav__route-item:contains("Fjölskyldan")').should('be.visible').click()
+  cy.get('.page-nav__route-item:contains("Barneignir")').should('be.visible').click()
+  cy.get('.page-nav__sub-route-item:contains("Fæðingar- og foreldraorlof")').should('be.visible').click()
+  cy.get('.text--h2').contains('Fæðingar- og foreldraorlof')
+})
